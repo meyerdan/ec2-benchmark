a9ae6e6eb192da83c8ed88dc143f3ff45fc91a74
Sebastian Menski <sebastian.menski@googlemail.com>
2014-11-28 13:24:05 +0100

    chore(curl): save curl output

--- scripts/start-process.sh
+++ scripts/start-process.sh
@@ -7,8 +7,13 @@ function start_process {
   local hostname=$1
   local proc_key=$2
   local instances=$3
+  if [ -e curl-log ]; then
+    rm -rf curl-log/*
+  else
+    mkdir -p curl-log
+  fi
   for i in $(seq 1 $instances); do
-    curl -sL -w "%{http_code} %{url_effective}\\n" -o /dev/null -H "Content-Type: application/json" -d "{}" http://${hostname}/engine-rest/process-definition/key/${proc_key}/start &
+    curl -sL -w "%{http_code} %{url_effective}\\n" -o curl-log/$hostname-$proc_key-$i.log -H "Content-Type: application/json" -d "{}" http://${hostname}/engine-rest/process-definition/key/${proc_key}/start &
   done
 }
 
