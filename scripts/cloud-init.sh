8685824486af62b50cd401ad2ff2803a2f644d28
Sebastian Menski <sebastian.menski@googlemail.com>
2014-11-28 10:18:37 +0100

    chore(docker): use adjusted camunda docker platform distro

--- scripts/cloud-init.sh
+++ scripts/cloud-init.sh
@@ -4,4 +4,4 @@ curl -sSL https://get.docker.com/ | sh
 
 usermod -aG docker ubuntu
 
-docker run -d -p 8080:8080 -e DB_JDBC="jdbc:postgresql://camunda-postgres.XXX.eu-central-1.rds.amazonaws.com:5432/process_engine" -e DB_USERNAME="camunda" -e DB_PASSWORD="camunda" menski/ec2-benchmark
+docker run -d -p 8080:8080 -e DB_DRIVER="org.postgresql.Driver" -e DB_JDBC="jdbc:postgresql://camunda-postgres.XXX.eu-central-1.rds.amazonaws.com:5432/process_engine" -e DB_USERNAME="camunda" -e DB_PASSWORD="camunda" menski/ec2-benchmark
